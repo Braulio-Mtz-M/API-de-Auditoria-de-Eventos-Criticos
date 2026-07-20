73970ac850714dff4f5fb6539a95a27d92345a98
Braulio Martinez <brauliomarmo@gmail>
2026-07-20 17:28:25 -0600

    Authorization

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 const verificarJWT = (req, res, next) => {
-    const authHeader = req.header('Authorization');
+    // 1. Cambiamos el nombre del header a 'app-token'
+    const token = req.header('app-token'); 
 
-    if (!authHeader) {
+    if (!token) {
         return res.status(401).json({ error: 'Acceso denegado. Se requiere un token.' });
     }
 
-    const token = authHeader.split(' ')[1];
-
     try {
+        // 2. Quitamos el .split(' ')[1] porque el token ya viene limpio, sin la palabra "Bearer"
         const decodificado = jwt.verify(token, process.env.JWT_SECRET);
         req.usuario = decodificado;
         next();
