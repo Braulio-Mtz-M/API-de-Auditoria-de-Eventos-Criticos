f5c5803f4cf5a8f3925ecf1f0a9dee1b4f6878ad
Braulio Martinez <brauliomarmo@gmail>
2026-07-20 21:20:50 -0600

    Helmet Implementation

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/database');
-
+const helmet = require('helmet');
 const auditoriaRoutes = require('./src/routes/auditoria');
 
 const app = express();
 const PORT = 5100;
 
+app.use(helmet());
 app.use(express.json());
 app.use('/api/auditorias', auditoriaRoutes);
 
